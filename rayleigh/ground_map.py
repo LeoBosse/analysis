7f66d8c04eb82d0c036019b3b0f042e6dca4797f
Leo Bosse <bosse.leo@laposte.net>
2020-04-09 16:13:43 +0200

    Improved ElevationMap object. Takes care of missing elevation data now. Few artifacts remain when adding two maps together.

--- rayleigh/ground_map.py
+++ rayleigh/ground_map.py
@@ -21,6 +21,9 @@ from rayleigh_utils import *
 
 class ElevationMap():
 	def __init__(self, in_dict):
+
+		# self.in_dict = in_dict
+
 		self.path = in_dict["alt_map_path"]
 
 		self.location = in_dict["location"]
@@ -28,15 +31,141 @@ class ElevationMap():
 		self.A_lon, self.A_lat = self.A_lon, self.A_lat
 		print(self.location, self.A_lon, self.A_lat)
 
+		self.nb_pix_max = int(in_dict["alt_map_N_bins_max"])
+		self.resolution = float(in_dict["alt_map_resolution"]) / RT #in radian
+		if self.resolution == 0:
+			self.resolution = (1/3600)*DtoR # 1 arcsec
+
 		self.radius = float(in_dict["alt_map_radius"]) / RT
 		self.N_bins_max = int(in_dict["ground_N_bins_max"])
+		self.lon_min, self.lon_max = self.A_lon - self.radius, self.A_lon + self.radius
+		self.lat_min, self.lat_max = self.A_lat - self.radius, self.A_lat + self.radius
+
 
 		print(self.radius)
 
-		self.LoadData()
+		self.SetMapProperties()
+
+		self.LoadMultipleTiles()
 
 		self.exist = (self.radius > 0)
 
+	def SetMapProperties(self):
+
+		self.nb_pix_lon = int((self.lon_max - self.lon_min) / self.resolution) + 1
+		self.nb_pix_lat = int((self.lat_max - self.lat_min) / self.resolution) + 1
+		self.nb_pix 	= self.nb_pix_lon * self.nb_pix_lat
+
+		if self.nb_pix > self.nb_pix_max and self.nb_pix_max > 0:
+			self.nb_pix_lon = int(np.sqrt(self.nb_pix_max))
+			self.nb_pix_lat = int(np.sqrt(self.nb_pix_max))
+			self.nb_pix 	= self.nb_pix_lon * self.nb_pix_lat
+			self.resolution = (self.lon_max - self.lon_min) / self.nb_pix_lon
+
+		self.lon_min -= self.lon_min % self.resolution
+		self.lon_max += self.resolution - self.lon_max % self.resolution
+
+		self.lat_min -= self.lat_min % self.resolution
+		self.lat_max += self.resolution - self.lat_max % self.resolution
+
+
+		self.map = np.zeros((self.nb_pix_lat, self.nb_pix_lon))
+
+		self.longitudes = np.linspace(self.lon_min, self.lon_max, self.nb_pix_lon)
+		self.latitudes  = np.linspace(self.lat_max, self.lat_min, self.nb_pix_lon)
+
+		self.lon_name_list = range(int(self.lon_min * RtoD), int(self.lon_max * RtoD) + 1,  1)
+		self.lat_name_list = range(int(self.lat_max * RtoD), int(self.lat_min * RtoD) - 1, -1)
+
+		self.nb_tile_lon = len(self.lon_name_list)
+		self.nb_tile_lat = len(self.lat_name_list)
+
+		self.nb_tiles = self.nb_tile_lon * self.nb_tile_lat
+
+		self.tiles_coord = []
+
+		for n_lon in self.lon_name_list:
+			for n_lat in self.lat_name_list:
+				self.tiles_coord.append((n_lon, n_lat))
+
+	def GetIndexFromLonLat(self, lon, lat):
+		line = int((self.lat_max - lat) / self.resolution)
+		col  = int((lon - self.lon_min) / self.resolution)
+
+		return line, col
+
+	def GetLonLatFromindex(self, l, c):
+		lon = self.longitudes[c]
+		lat = self.latitude[l]
+
+		return lon, lat
+
+
+	def LoadSingleTile(self, n_lon, n_lat):
+		file_name = self.GetNameFromLonLat(n_lon, n_lat)
+		print(file_name)
+
+		try:
+			tile = gdal.Open(self.path + file_name, gdal.GA_ReadOnly) #get raster objsect from tif file
+		except:
+			print("WARNING: Elevation tile not found:", self.path + file_name)
+			return np.zeros((1,1)), self.A_lon, self.A_lat
+
+		nb_lon = tile.RasterXSize
+		nb_lat = tile.RasterYSize
+
+		tile_info = tile.GetGeoTransform()
+
+		print("tile info", tile_info)
+		origin_lon = tile_info[0]	 * DtoR
+		origin_lat = tile_info[3]	 * DtoR
+		pixel_width = tile_info[1]	 * DtoR
+		pixel_height = tile_info[5]	 * DtoR
+
+		LonToCol = lambda l: int((l - origin_lon) / pixel_width)
+		LatToRow = lambda l: int((l - origin_lat) / pixel_height)
+
+		lon_min = max(self.A_lon - self.radius, origin_lon)
+		lon_max = min(self.A_lon + self.radius, origin_lon + pixel_width * nb_lon)
+
+		lat_min = max(self.A_lat - self.radius, origin_lat + pixel_height * nb_lat)
+		lat_max = min(self.A_lat + self.radius, origin_lat)
+
+		nb_lon  = int((lon_max - lon_min) / pixel_width)
+		nb_lat  = int((lat_min - lat_max) / pixel_height)
+
+		tile = tile.GetRasterBand(1).ReadAsArray(LonToCol(lon_min), LatToRow(lat_max), nb_lon, nb_lat)
+
+		if pixel_width != self.resolution:
+			nb_lon  = int((lon_max - lon_min) / self.resolution)
+			nb_lat  = int((lat_max - lat_min) / self.resolution)
+
+			tile = cv2.resize(tile, dsize=(nb_lon, nb_lat), interpolation = cv2.INTER_CUBIC)
+
+		return tile, lon_min, lat_max
+
+	def LoadMultipleTiles(self):
+
+		for tile_lon, tile_lat in self.tiles_coord:
+			tile, lon_min, lat_max = self.LoadSingleTile(tile_lon, tile_lat)
+
+			tile_line, tile_col = self.GetIndexFromLonLat(lon_min, lat_max)
+			nb_lat, nb_lon = tile.shape
+
+			print("tile.shape", tile.shape)
+			print("map chunk shape", self.map[tile_line : tile_line + nb_lat, tile_col : tile_col + nb_lon].shape)
+
+			print(self.map.shape, tile_line, tile_line + nb_lat, tile_col, tile_col + nb_lon)
+
+			self.map[tile_line : tile_line + nb_lat, tile_col : tile_col + nb_lon] += tile
+
+
+	def PlotMap(self):
+		f = plt.figure()
+		plt.pcolormesh((self.longitudes - self.A_lon) * RT, (self.latitudes - self.A_lat) * RT, self.map)
+		# plt.pcolormesh(np.flip(self.map, axis=0))
+		# plt.pcolormesh(np.flip(np.rot90(self.map, k=-1)))
+		plt.show()
 
 	def GetNameFromLonLat(self, lon, lat):
 		lon = int(lon)
@@ -64,113 +193,6 @@ class ElevationMap():
 		name += str(int(lon)).zfill(3)
 
 		return name
-
-	def LoadData(self):
-		min_lon, min_lat = self.A_lon - self.radius, self.A_lat - self.radius
-		max_lon, max_lat = self.A_lon + self.radius, self.A_lat + self.radius
-		print(min_lon, min_lat, max_lon, max_lat)
-
-		lon_name_list = range(int(min_lon * RtoD), int(max_lon * RtoD) + 1,  1)
-		lat_name_list = range(int(max_lat * RtoD), int(min_lat * RtoD) - 1, -1)
-
-		self.map = None
-		for n_lon in lon_name_list:
-			lon_band = None
-			old_tile_nb_lon = 0
-			for n_lat in lat_name_list:
-
-				file_name = self.GetNameFromLonLat(n_lon, n_lat)
-				print(file_name)
-				tile = gdal.Open(self.path + file_name, gdal.GA_ReadOnly) #get raster objsect from tif file
-
-				nb_lon = tile.RasterXSize
-				nb_lat = tile.RasterYSize
-
-				tile_info = tile.GetGeoTransform()
-
-				print("tile info", tile_info)
-				origin_lon = tile_info[0]	 * DtoR
-				origin_lat = tile_info[3]	 * DtoR
-				pixel_width = tile_info[1]	 * DtoR
-				pixel_height = tile_info[5]	 * DtoR
-
-				LonToCol = lambda l: int((l - origin_lon) / pixel_width)
-				LatToRow = lambda l: int((l - origin_lat) / pixel_height)
-
-				lon_min = max(self.A_lon - self.radius, origin_lon)
-				lon_max = min(self.A_lon + self.radius, origin_lon + pixel_width * nb_lon)
-
-				lat_min = max(self.A_lat - self.radius, origin_lat + pixel_height * nb_lat)
-				lat_max = min(self.A_lat + self.radius, origin_lat)
-
-				nb_lon  = int((lon_max - lon_min) / pixel_width)
-				nb_lat  = int((lat_min - lat_max) / pixel_height)
-
-				print("GROUND MAP", nb_lon, nb_lat)
-
-				self.longitudes, self.dlon = np.linspace(lon_min, lon_max, nb_lon, retstep=True) # list of pixel longitudes
-				self.latitudes, self.dlat = np.linspace(lat_max, lat_min, nb_lat, retstep=True) # list of pixel latitudes
-
-				tile = tile.GetRasterBand(1).ReadAsArray(LonToCol(lon_min), LatToRow(lat_max), nb_lon, nb_lat)
-
-				print(tile)
-				tile_nb_lat, new_tile_nb_lon = tile.shape
-
-				if old_tile_nb_lon != 0:
-					if old_tile_nb_lon != new_tile_nb_lon:
-						tile = cv2.resize(tile, dsize=(old_tile_nb_lon, tile_nb_lat), interpolation = cv2.INTER_CUBIC)
-
-				else:
-					old_tile_nb_lon = new_tile_nb_lon
-
-				if lon_band is not None:
-					lon_band = np.append(lon_band, tile, axis=0)
-				else:
-					lon_band = tile
-
-
-			if self.map is not None and lon_band.size > 0:
-				self.map = np.append(self.map, lon_band, axis=1)
-			else:
-				self.map = lon_band
-
-			print("MAP SHAPE", self.map.shape)
-
-
-		self.map = np.flip(self.map, axis=0)
-		nb_lat, nb_lon = self.map.shape
-
-		self.longitudes, self.dlon = np.linspace(min_lon, max_lon, nb_lon, endpoint=True, retstep = True)
-		self.latitudes, self.dlat = np.linspace(max_lat, min_lat, nb_lat, endpoint=True, retstep = True)
-
-		print(self.longitudes, self.latitudes)
-
-
-
-		if nb_lon * nb_lat > self.N_bins_max and self.N_bins_max > 0:
-			print("GROUND MAP TOO BIG", nb_lon * nb_lat, self.N_bins_max)
-			new_nb_lon, new_nb_lat = int(np.sqrt(self.N_bins_max)), int(np.sqrt(self.N_bins_max))
-			print("GROUND MAP", new_nb_lon, new_nb_lat)
-
-			self.map = cv2.resize(self.map, dsize=(new_nb_lon, new_nb_lat), interpolation = cv2.INTER_CUBIC)
-			nb_lat, nb_lon = self.map.shape
-
-			self.longitudes, self.dlon = np.linspace(min_lon, max_lon, nb_lon, retstep=True) # list of pixel longitudes
-			self.latitudes, self.dlat = np.linspace(max_lat, min_lat, nb_lat, retstep=True) # list of pixel latitudes
-
-
-
-		self.exist = True
-		if not self.map.shape:
-			self.exist = True
-
-
-	def PlotMap(self):
-		f = plt.figure()
-		plt.pcolormesh(self.map)
-		# plt.pcolormesh(np.flip(self.map, axis=0))
-		# plt.pcolormesh(np.flip(np.rot90(self.map, k=-1)))
-		plt.show()
 
 
 class GroundMap:
